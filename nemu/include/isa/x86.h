e79b0884a1c843a19571dbaae98ca6513d64342b
tracer-ics2020 <tracer@njuics.org>
2020-09-22 00:27:21 +0800

    >  compile 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: ba296e5a22a142c6bd15f68ee98ff290   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-47-generic       Architecture: x86-64  00:27:21 up 16:20,  1 user,  load average: 0.84, 0.37, 0.17

--- nemu/include/isa/x86.h
+++ nemu/include/isa/x86.h
@@ -20,9 +20,13 @@
 typedef struct {
   union{
     struct {
-      uint32_t _32;
-      uint16_t _16;
-      uint8_t _8[2];
+      union{
+        uint32_t _32;
+        union{
+          uint16_t _16;
+          uint8_t _8[2];
+          };
+        };
     }gpr[8];
   /* Do NOT change the order of the GPRs' definitions. */
 
