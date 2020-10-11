ed914797a02141fcdd973b987ced9ebb53e26ed4
tracer-ics2020 <tracer@njuics.org>
2020-10-11 10:35:42 +0800

    >  compile 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 39327100ce7a476d943259429912e8ee   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  10:35:42 up 1 day, 10:23,  1 user,  load average: 0.35, 0.22, 0.13

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -301,12 +301,13 @@ word_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
   //done 
   for (int i = 0; i < nr_token; i ++) {
-    printf("%d  %s\n",tokens[i].type,tokens[i].str);
+    //printf("%d  %s\n",tokens[i].type,tokens[i].str);
   if (tokens[i].type == '-' && (i == 0 || tokens[i - 1].type == '+'|| tokens[i - 1].type == '-'||tokens[i - 1].type == '*'|| tokens[i - 1].type == '/'||tokens[i - 1].type == '('|| tokens[i - 1].type == TK_MINUS) ) {
     tokens[i].type = TK_MINUS;
-    assert(0);
+    //assert(0);
   }
   }
+  assert(0);
   if(legal_exp(0,nr_token-1)) return eval(0,nr_token-1);
   else {
     *success = false; 
