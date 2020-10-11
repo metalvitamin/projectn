2e114e88dc3ed2ad11bbc68a5948cc01ab683668
tracer-ics2020 <tracer@njuics.org>
2020-10-11 18:08:25 +0800

    >  compile 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 39327100ce7a476d943259429912e8ee   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  18:08:25 up 1 day, 17:56,  1 user,  load average: 1.32, 0.40, 0.18

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -60,7 +60,7 @@ typedef struct token {
   char str[32];
 } Token;
 
-static Token tokens[128] __attribute__((used)) = {};
+static Token tokens[64] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 static inline void init_token(int o){
   tokens[nr_token].type = o;
