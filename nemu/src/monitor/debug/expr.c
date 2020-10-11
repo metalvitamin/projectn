c07178a670d22b4a223146171c9e2cbf04b9f91e
tracer-ics2020 <tracer@njuics.org>
2020-10-11 11:26:36 +0800

    >  compile 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 39327100ce7a476d943259429912e8ee   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-48-generic       Architecture: x86-64  11:26:37 up 1 day, 11:14,  1 user,  load average: 0.23, 0.27, 0.26

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -290,6 +290,7 @@ static uint32_t eval(int p, int q){
     uint32_t val1 = eval(p , op - 1), val2 = eval( op + 1 , q);
     //assert(0);
     assert(op <nr_token);
+    return val1+val2;
     switch(tokens[op].type){
       case('+'):return val1+val2;
       case('-'):return val1-val2;
