3d5c23f696748cff9e31243c3fa92250e55c8146
tracer-ics2020 <tracer@njuics.org>
2020-10-29 22:06:13 +0800

    >  compile 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 6495aeba71f442909a902defc158c246   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-51-generic       Architecture: x86-64  22:06:13 up  3:18,  1 user,  load average: 0.63, 0.42, 0.25

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -41,6 +41,7 @@ static int cmd_q(char *args) {
 static int cmd_help(char *args);
 
 static int cmd_si(char *args){
+  if(args == NULL) cpu_exec(1);
   cpu_exec(atoi(args));
   return 0;
 }
@@ -67,6 +68,12 @@ static int cmd_p(char*args){
   printf("Wrong expression\n");
   return 0;
 }
+/*static int cmd_w(char* args){// todo
+  return 0;
+}
+static int cmd_d(char* args){
+  return 0;
+}*/
 static struct {
   char *name;
   char *description;
