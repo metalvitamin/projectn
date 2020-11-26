0a04d3e8e8c635abb71d7fb334b4e01f01385e6d
tracer-ics2020 <tracer@njuics.org>
2020-11-27 01:03:47 +0800

    >  compile 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: d8d3a201425a4c06825b81719ffea668   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-53-generic       Architecture: x86-64  01:03:47 up 1 day, 12:32,  1 user,  load average: 0.74, 0.46, 0.39

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -64,7 +64,7 @@ static inline def_EHelper(2byte_esc) {
   switch (opcode) {
   /* TODO: Add more instructions!!! */
     IDEX (0x01, gp7_E, gp7)
-    IDEXW(0xb6, E2G, movzx, 1)
+    IDEXW(0xb6, mov_E2G, movzx, 1)
     IDEXW(0x94, setcc_E, setcc, 1)
     default: exec_inv(s);
   }
