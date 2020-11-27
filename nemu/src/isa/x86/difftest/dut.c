c46244461bda300c39f75ce45ecaffc0f90fb046
tracer-ics2020 <tracer@njuics.org>
2020-11-27 23:22:45 +0800

    >  compile 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 7a302ca25f034b53a38590eff77b8da7   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-53-generic       Architecture: x86-64  23:22:45 up  5:59,  1 user,  load average: 0.18, 0.31, 0.15

--- nemu/src/isa/x86/difftest/dut.c
+++ nemu/src/isa/x86/difftest/dut.c
@@ -6,6 +6,7 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i = 0; i < 8; i ++){
     if(ref_r->gpr[i]._32 != cpu.gpr[i]._32){
+      printf("wrong reg index %d = %u",i,ref_r->gpr[i]._32);
       return false;
     }
   }
