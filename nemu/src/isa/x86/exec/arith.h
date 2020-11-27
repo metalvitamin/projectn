b7c40d0bfa5d1eb10029e3cc6a999b4bb875b351
tracer-ics2020 <tracer@njuics.org>
2020-11-28 01:01:40 +0800

    >  compile 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 7a302ca25f034b53a38590eff77b8da7   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-53-generic       Architecture: x86-64  01:01:41 up  7:38,  1 user,  load average: 0.57, 0.92, 0.76

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -45,14 +45,25 @@ static inline def_EHelper(cmp) {
 }
 
 static inline def_EHelper(inc) {
-  rtl_addi(s,ddest,ddest,1);
-  operand_write(s, id_dest, ddest);
+  rtl_addi(s,s0,ddest,1);
+  rtl_update_ZFSF(s, s0, id_dest->width);
+  rtl_is_add_carry(s, s1, s0, dsrc1);
+  rtl_set_CF(s, s1);
+  rtl_is_add_overflow(s, s1, s0, ddest, dsrc1, id_dest->width);
+  rtl_set_OF(s, s1);
+  operand_write(s, id_dest, s0);
+  operand_write(s, id_dest, s0);
   print_asm_template1(inc);
 }
 
 static inline def_EHelper(dec) {
-  rtl_subi(s,ddest,ddest,1);
-  operand_write(s, id_dest, ddest);
+  rtl_subi(s,s0,ddest,1);
+  rtl_update_ZFSF(s, s0, id_dest->width);
+  rtl_is_sub_carry(s, s1, ddest, dsrc1);
+  rtl_set_CF(s, s1);
+  rtl_is_sub_overflow(s, s1, s0, ddest, dsrc1, id_dest->width);
+  rtl_set_OF(s, s1);
+  operand_write(s, id_dest, s0);
   print_asm_template1(dec);
 }
 
