5b04a9244376a3622f5b3cf49d2edab83025813c
tracer-ics2020 <tracer@njuics.org>
2020-11-27 22:24:45 +0800

    >  compile 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 7a302ca25f034b53a38590eff77b8da7   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-53-generic       Architecture: x86-64  22:24:46 up  5:01,  1 user,  load average: 0.37, 0.33, 0.27

--- nemu/src/isa/x86/exec/arith.h
+++ nemu/src/isa/x86/exec/arith.h
@@ -5,6 +5,7 @@ static inline def_EHelper(add) {
   rtl_update_ZFSF(s, s0, id_dest->width);
   rtl_is_add_carry(s, s1, ddest, dsrc1);
   rtl_set_CF(s, s1);
+  printf("\n\nCF = %u\n\n",*s1);
   rtl_is_add_overflow(s, s1, s0, ddest, dsrc1, id_dest->width);
   rtl_set_OF(s, s1);
   operand_write(s, id_dest, s0);
