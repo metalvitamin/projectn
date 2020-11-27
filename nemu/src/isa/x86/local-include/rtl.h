6aa392ead341c57d25ce75cedaceff09df712054
tracer-ics2020 <tracer@njuics.org>
2020-11-27 22:16:43 +0800

    >  compile 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 7a302ca25f034b53a38590eff77b8da7   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-53-generic       Architecture: x86-64  22:16:43 up  4:53,  1 user,  load average: 0.15, 0.12, 0.23

--- nemu/src/isa/x86/local-include/rtl.h
+++ nemu/src/isa/x86/local-include/rtl.h
@@ -108,7 +108,7 @@ static inline def_rtl(is_add_carry, rtlreg_t* dest,
 #define cpueflagsSF cpu.eflags.SF
 #define def_rtl_setget_eflags(f) \
   static inline def_rtl(concat(set_, f), const rtlreg_t* src) { \
-    concat(cpueflags,f)= *src; \
+    concat(cpueflags,f) = *src; \
   } \
   static inline def_rtl(concat(get_, f), rtlreg_t* dest) { \
     *dest = concat(cpueflags,f); \
