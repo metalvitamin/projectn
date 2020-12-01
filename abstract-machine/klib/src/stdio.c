67bf9123c8e24f7ce6191a0bae9da86915d39dc5
tracer-ics2020 <tracer@njuics.org>
2020-12-02 00:26:15 +0800

    >  run 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 763cb63ebdf94f779d521b4ee617cf55   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-53-generic       Architecture: x86-64  00:26:15 up  3:32,  1 user,  load average: 0.21, 0.35, 0.37

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,21 +14,24 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
+  *out = '\0';
   va_list ap;
   va_start(ap,fmt);
-  for(int i = 0; fmt[i + 1] != '\0'; i ++){
+  for(int i = 0; fmt[i] != '\0'; i ++){
     if(fmt[i] != '%'){
       char ch[2] = {fmt[i]};
       strcat(out,ch);
-      continue;
     }
-    else
+    else if(fmt[i + 1] != '\0')
     {
       char* ch;
       int d;
+      char chtemp[12] = {"+0000000000"};
       switch (fmt[i + 1])
       {
       case '%':
+        ch = "%";
+        strcat(out,ch);
         break;
       case 's':
         ch = va_arg(ap,char*);
@@ -37,33 +40,43 @@ int sprintf(char *out, const char *fmt, ...) {
       case 'd':
         
         d = va_arg(ap, int);
-
-        char chtemp[12] = {"+0000000000"};
         if(d < 0) chtemp[0] = '-';
-        for(int i = 11; i > 0; i --){
+        for(int i = 10; i > 0; i --){
           int temp = d % 10;
-          if (temp < 0)temp = -temp;
-          chtemp[i] = temp - '0';
+          if (temp < 0) temp = -temp;
+          chtemp[i] = temp + '0';
           d = d / 10;
           if(d == 0) break;
         }
+        //putch(chtemp[0]);putch(chtemp[1]);putch(chtemp[2]);putch(chtemp[3]);putch(chtemp[4]);putch(chtemp[5]);putch(chtemp[6]);putch(chtemp[7]);putch(chtemp[8]);putch(chtemp[9]);putch(chtemp[10]);putch('\n');
         ch = chtemp;
         for(int i = 1; i < 11; i ++){
-          if(chtemp[i] != 0){
-            chtemp[i - 1] = '-';
-            ch = &chtemp[i - 1];
+          //putch(chtemp[i]);putch('\n');
+          if(chtemp[i] != '0'){
+            chtemp[i - 1] = chtemp[0];
+            if(chtemp[0] == '+'){
+              //putch('+');
+              //putch(chtemp[i]);
+              ch = &chtemp[i];
+            }
+            else if (chtemp[0] == '-'){
+              //putch('-');
+              ch = &chtemp[i - 1];
+            }
             break;
           }
         }
+        //putch(*ch);putch('\n');
         strcat(out,ch);
         break;
-      default:
-        break;
+      
       }
+      
+      i ++;
     }
     
   }
-  
+  //putch(out[0]);putch(out[1]);putch(out[2]);putch(out[3]);putch(out[4]);putch(out[5]);putch(out[6]);putch(out[7]);putch(out[8]);putch(out[9]);putch(out[10]);
   //va_arg(ap,char);
   va_end(ap);
   return 0;
