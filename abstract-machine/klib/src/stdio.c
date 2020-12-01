522c739f9406ed7e7cf63c31a91736707522c2df
tracer-ics2020 <tracer@njuics.org>
2020-12-01 23:51:14 +0800

    >  run 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 763cb63ebdf94f779d521b4ee617cf55   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-53-generic       Architecture: x86-64  23:51:14 up  2:57,  1 user,  load average: 0.41, 0.32, 0.21

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,6 +14,58 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap,fmt);
+  for(int i = 0; fmt[i + 1] != '\0'; i ++){
+    if(fmt[i] != '%'){
+      char ch[2] = {fmt[i]};
+      strcat(out,ch);
+      continue;
+    }
+    else
+    {
+      char* ch;
+      int d;
+      switch (fmt[i + 1])
+      {
+      case '%':
+        break;
+      case 's':
+        ch = va_arg(ap,char*);
+        strcat(out,ch);
+        break;
+      case 'd':
+        
+        d = va_arg(ap, int);
+
+        char chtemp[12] = {"+0000000000"};
+        if(d < 0) chtemp[0] = '-';
+        for(int i = 11; i > 0; i --){
+          int temp = d % 10;
+          if (temp < 0)temp = -temp;
+          chtemp[i] = temp - '0';
+          d = d / 10;
+          if(d == 0) break;
+        }
+        ch = chtemp;
+        for(int i = 1; i < 11; i ++){
+          if(chtemp[i] != 0){
+            chtemp[i - 1] = '-';
+            ch = &chtemp[i - 1];
+            break;
+          }
+        }
+        strcat(out,ch);
+        break;
+      default:
+        break;
+      }
+    }
+    
+  }
+  
+  //va_arg(ap,char);
+  va_end(ap);
   return 0;
 }
 
