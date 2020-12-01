9ba3bb086668c04640a1a5974b3328939ca6823c
tracer-ics2020 <tracer@njuics.org>
2020-12-02 00:29:43 +0800

    >  compile 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 763cb63ebdf94f779d521b4ee617cf55   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-53-generic       Architecture: x86-64  00:29:43 up  3:36,  1 user,  load average: 0.99, 0.85, 0.58

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -195,6 +195,7 @@ again:
     IDEX (0xd3, gp2_cl2E, gp2)
     EX   (0xd6, nemu_trap)
     IDEX (0xe8, J, call)
+    IDEX (0xe9, J, jmp)
     IDEXW(0xeb, J, jmp, 1)
     IDEXW(0xf6, E, gp3, 1)
     IDEX (0xf7, E, gp3)
