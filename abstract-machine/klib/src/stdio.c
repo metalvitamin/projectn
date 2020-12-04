a27630bef075806aa3e93bbe5d3ad833a0c4c077
tracer-ics2020 <tracer@njuics.org>
2020-12-05 06:22:12 +0800

    >  compile 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 8b2901c0c4704f338d391f4157b8fb39   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  06:22:12 up  6:14,  1 user,  load average: 0.43, 0.40, 0.31

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,7 +5,68 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+
+static inline void dec_int(char* ch, int d){
+  char chtemp[12] = {"+0000000000"};
+  if(d < 0) chtemp[0] = '-';
+  for(int i = 10; i > 0; i --){
+    int temp = d % 10;
+    if (temp < 0) temp = -temp;
+    chtemp[i] = temp + '0';
+    d = d / 10;
+    if(d == 0) {
+      chtemp[i - 1] = chtemp[0];
+      if(chtemp[0] == '+'){
+        ch = &chtemp[i];
+      }
+      else {
+        ch = &chtemp[i - 1];
+      }
+      break;
+    }
+  }
+}
+static inline void sec_stream(char* ch){
+  for(int i = 0; ch[i] != '\0'; i ++){
+    putch(ch[i]);
+  }
+}
+
 int printf(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap,fmt);
+  for(int i = 0; fmt[i] != '\0'; i ++){
+    if(fmt[i] != '%'){
+      putch(fmt[i]);
+    }
+    else if(fmt[i + 1] != '\0')
+    {
+      char* ch;
+      int d;
+      switch (fmt[i + 1])
+      {
+      case '%':
+        ch = "%";
+        sec_stream(ch);
+        break;
+      case 's':
+        ch = va_arg(ap,char*);
+        sec_stream(ch);
+        break;
+      case 'd':
+        ch = "";
+        d = va_arg(ap, int);
+        dec_int(ch,d);
+        sec_stream(ch);
+        break;
+      
+      }
+      
+      i ++;
+    }
+    
+  }
+  va_end(ap);
   return 0;
 }
 
@@ -26,7 +87,6 @@ int sprintf(char *out, const char *fmt, ...) {
     {
       char* ch;
       int d;
-      char chtemp[12] = {"+0000000000"};
       switch (fmt[i + 1])
       {
       case '%':
@@ -38,35 +98,9 @@ int sprintf(char *out, const char *fmt, ...) {
         strcat(out,ch);
         break;
       case 'd':
-        
+        ch = "";
         d = va_arg(ap, int);
-        if(d < 0) chtemp[0] = '-';
-        for(int i = 10; i > 0; i --){
-          int temp = d % 10;
-          if (temp < 0) temp = -temp;
-          chtemp[i] = temp + '0';
-          d = d / 10;
-          if(d == 0) break;
-        }
-        //putch(chtemp[0]);putch(chtemp[1]);putch(chtemp[2]);putch(chtemp[3]);putch(chtemp[4]);putch(chtemp[5]);putch(chtemp[6]);putch(chtemp[7]);putch(chtemp[8]);putch(chtemp[9]);putch(chtemp[10]);putch('\n');
-        ch = chtemp;
-        for(int i = 1; i < 11; i ++){
-          //putch(chtemp[i]);putch('\n');
-          if(chtemp[i] != '0'){
-            chtemp[i - 1] = chtemp[0];
-            if(chtemp[0] == '+'){
-              //putch('+');
-              //putch(chtemp[i]);
-              ch = &chtemp[i];
-            }
-            else if (chtemp[0] == '-'){
-              //putch('-');
-              ch = &chtemp[i - 1];
-            }
-            break;
-          }
-        }
-        //putch(*ch);putch('\n');
+        dec_int(ch,d);
         strcat(out,ch);
         break;
       
@@ -76,8 +110,6 @@ int sprintf(char *out, const char *fmt, ...) {
     }
     
   }
-  //putch(out[0]);putch(out[1]);putch(out[2]);putch(out[3]);putch(out[4]);putch(out[5]);putch(out[6]);putch(out[7]);putch(out[8]);putch(out[9]);putch(out[10]);
-  //va_arg(ap,char);
   va_end(ap);
   return 0;
 }
