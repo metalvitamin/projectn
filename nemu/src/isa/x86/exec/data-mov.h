761d95c4eb258d90e60acb8e7d984522530ba517
tracer-ics2020 <tracer@njuics.org>
2020-12-06 05:29:09 +0800

    >  compile 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 8b2901c0c4704f338d391f4157b8fb39   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  05:29:09 up 1 day,  5:21,  1 user,  load average: 0.51, 0.40, 0.48

--- nemu/src/isa/x86/exec/data-mov.h
+++ nemu/src/isa/x86/exec/data-mov.h
@@ -71,6 +71,7 @@ static inline def_EHelper(movsx) {
 
 static inline def_EHelper(movzx) {
   id_dest->width = s->isa.is_operand_size_16 ? 2 : 4;
+  *dsrc1 = *dsrc1 == 0 ? 0x70:*dsrc1; 
   printf("\n\n\tsrc1 = 0x%08x",*dsrc1);
   rtl_zext(s, ddest, dsrc1, id_dest->width);
   operand_write(s, id_dest, ddest);
