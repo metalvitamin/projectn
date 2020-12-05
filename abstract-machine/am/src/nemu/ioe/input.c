8b5b74bfce0da6748c0916d4fb2da5c096f6ab75
tracer-ics2020 <tracer@njuics.org>
2020-12-06 07:30:26 +0800

    >  run 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 8b2901c0c4704f338d391f4157b8fb39   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  07:30:26 up 1 day,  7:23,  1 user,  load average: 0.83, 0.87, 0.81

--- abstract-machine/am/src/nemu/ioe/input.c
+++ abstract-machine/am/src/nemu/ioe/input.c
@@ -4,6 +4,8 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  int temp = inl(KBD_ADDR);
+
+  kbd->keycode = temp ^ KEYDOWN_MASK;
+  kbd->keydown = temp ^ kbd->keycode;
 }
