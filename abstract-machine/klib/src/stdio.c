9ac93d3b13c0aa5c562ddc4b35e95b261af568d7
tracer-ics2020 <tracer@njuics.org>
2020-12-05 21:57:47 +0800

    >  run 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 8b2901c0c4704f338d391f4157b8fb39   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  21:57:47 up 21:50,  1 user,  load average: 0.95, 0.62, 0.34

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -49,7 +49,7 @@ format:\
         exec;\
         break;\
       case '0':case '1':case '2':case '3':case '4':case '5':case '6':case '7':case '8':case '9':\
-        min_broad = fmt[i];\
+        min_broad = fmt[i] - '0';\
         goto format;\
       }\
     }
