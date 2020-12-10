3e3f6c42d085b5b509814b95fbe628619cb3b615
tracer-ics2020 <tracer@njuics.org>
2020-12-10 20:35:02 +0800

    >  compile 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 44b8138c170144218292515758efeba9   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  20:35:02 up 13:09,  1 user,  load average: 0.65, 0.51, 0.61

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -27,36 +27,40 @@ static inline def_rtl(neg, rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline def_rtl(sext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
+  int32_t t;
   if(width == 1){
     int8_t tem = *src1;
-    *dest = tem;
+    t = tem;
   }
   else if(width == 2)
   {
     int16_t tem = *src1;
-    *dest = tem;
+    t = tem;
   }else{
     int32_t tem = *src1;
-    *dest = tem;
+    t = tem;
   }
   
+  *dest = t;
 }
 
 static inline def_rtl(zext, rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- zeroext(src1[(width * 8 - 1) .. 0])
+  uint32_t u;
   if(width == 1){
     uint8_t tem = *src1;
-    *dest = tem;
+    u = tem;
   }
   else if(width == 2)
   {
 
     uint16_t tem = *src1;
-    *dest = tem;
+    u = tem;
   }else{
     uint32_t tem = *src1;
-    *dest = tem;
+    u = tem;
   }
+  *dest = u;
   
 
 }
