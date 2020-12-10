dfc285a98113089e74031d73682c6945ca3b3223
tracer-ics2020 <tracer@njuics.org>
2020-12-10 18:04:01 +0800

    >  run 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 44b8138c170144218292515758efeba9   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  18:04:01 up 10:38,  1 user,  load average: 0.34, 0.40, 0.29

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -21,6 +21,16 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+
+  int height = inl(VGACTL_ADDR) & 0xff;
+  uint32_t *p = ctl->pixels;
+
+  for(int i = 0; i < ctl->w; i ++){
+    for(int j = 0; j < ctl->h; j ++){
+      uint32_t position = height * (ctl->x + i) + (ctl->y + j);
+      outl(FB_ADDR + position * 4, p[position]);
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
