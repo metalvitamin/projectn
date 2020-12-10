def2547ef87ee3e83f2c792293c3653ed9a47313
tracer-ics2020 <tracer@njuics.org>
2020-12-10 23:57:50 +0800

    >  run 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 44b8138c170144218292515758efeba9   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-56-generic       Architecture: x86-64  23:57:50 up 16:32,  1 user,  load average: 0.17, 0.20, 0.18

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -22,10 +22,10 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int width = io_read(AM_GPU_CONFIG).width;
-  //int height = io_read(AM_GPU_CONFIG).height;
+  int height = io_read(AM_GPU_CONFIG).height;
   uint32_t *p = (uint32_t *)ctl->pixels;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  fb = fb + (width * ctl->x) + ctl->y;
+  fb = fb + (height * ctl->x) + ctl->y;
   for(int i = 0; i < ctl->w; i ++){
     for (int j = 0; j < ctl->h; j++)
     {
