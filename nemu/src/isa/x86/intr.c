42fd7da7479476cba5914bf5aa2e2880b9f8e5af
tracer-ics2020 <tracer@njuics.org>
2020-12-26 00:48:18 +0800

    >  compile 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 5fcbedfde79b4b93a5849295b0f3e90f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  00:48:18 up 5 days,  2:03,  1 user,  load average: 1.18, 0.45, 0.30

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -7,7 +7,7 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
    */
   //printf("IDTR = %u\n", cpu.IDTR.idt);
   printf("in cpu before:\n");
-  printf("eflags = %d, cs = %d, eip = %d", cpu.eflags.EFLAGS, cpu.cs, cpu.pc);
+  printf("eflags = %d, cs = %d, eip = %d\n", cpu.eflags.EFLAGS, cpu.cs, cpu.pc);
   rtl_push(s, &cpu.eflags.EFLAGS);
   rtl_push(s, &cpu.cs);
   rtl_push(s, &ret_addr);
@@ -18,7 +18,7 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
   uint8_t valid = (*s1 >> 15) & 0x1;
   if (valid == 1){
     vaddr_t gate = (*s0 & 0xffff) |(*s1 & ~0xffff);
-    //printf("gate = %x\n", gate);
+    printf("gate = %x\n", gate);
     rtl_j(s,gate);
   }
   else
