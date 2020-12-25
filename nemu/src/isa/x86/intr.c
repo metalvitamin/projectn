a9c586c01551f35e4769b248df7b8affca44b278
tracer-ics2020 <tracer@njuics.org>
2020-12-25 19:27:12 +0800

    >  compile 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 5fcbedfde79b4b93a5849295b0f3e90f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  19:27:12 up 4 days, 20:42,  1 user,  load average: 0.32, 0.21, 0.31

--- nemu/src/isa/x86/intr.c
+++ nemu/src/isa/x86/intr.c
@@ -5,11 +5,11 @@ void raise_intr(DecodeExecState *s, uint32_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-  vaddr_t access = cpu.IDTR.idt + NO * cpu.IDTR.length / 0x100;
-  assert(0);
-  uint32_t first = vaddr_read(access ++, 4);
-  assert(0);
-  uint32_t last = vaddr_read(access, 4);
+  vaddr_t access = cpu.IDTR.idt + NO * cpu.IDTR.length / 256;
+
+  uint32_t first = vaddr_read(access, 4);
+
+  uint32_t last = vaddr_read(access + 4, 4);
   uint8_t valid = (last >> 15) & 0x1;
   if (valid == 1){
     vaddr_t gate = (first & 0xffff) |(last & ~0xffff);
