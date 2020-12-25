a9561bf43e2f213974d1524e9f7db1469732aeda
tracer-ics2020 <tracer@njuics.org>
2020-12-26 00:56:46 +0800

    >  run 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 5fcbedfde79b4b93a5849295b0f3e90f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  00:56:46 up 5 days,  2:11,  1 user,  load average: 0.62, 0.57, 0.40

--- abstract-machine/am/include/arch/x86-nemu.h
+++ abstract-machine/am/include/arch/x86-nemu.h
@@ -2,10 +2,10 @@
 #define ARCH_H__
 
 struct Context {
-  
-  
+  void *cr3;
+  int irq;
   uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
-  int irq;void *cr3;
+  
   uintptr_t eip, cs, eflags;
 };
 
