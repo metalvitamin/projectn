2a79fbff291639c54ebf1d8ff9e8eb3cb2a03ba4
tracer-ics2020 <tracer@njuics.org>
2020-12-27 23:33:01 +0800

    >  run 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: c1a0515ffd294d459f6c7758f260df8f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  23:33:01 up 1 day, 47 min,  1 user,  load average: 0.10, 0.27, 0.21

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -15,7 +15,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr ehdr;
   
   ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
-  size_t phdraddr = ehdr.e_phoff;
+  uint64_t phdraddr = ehdr.e_phoff;
   printf("type = %d\n",ehdr.e_type);
   printf("poffset = 0x%x, phnum = %d, ehsize = 0x%x\n",ehdr.e_phoff, ehdr.e_phnum,ehdr.e_ehsize);
   printf("vaddr = 0x%x\n", ehdr.e_entry);
@@ -23,8 +23,6 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   int count = ehdr.e_phnum;
   Elf_Phdr *phdr[ehdr.e_phnum];ramdisk_read(phdr[0], phdraddr, sizeof(Elf_Phdr));
   for(int i = 0; i < count; i ++){
-    
-    
     putch('\n');putch('\n');putch('\n');
     if(phdr[0]->p_type == PT_LOAD){
       size_t buf[phdr[0]->p_filesz];
@@ -36,9 +34,9 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       memset(zero, 0, phdr[0]->p_memsz - phdr[0]->p_filesz);
       entrance += phdr[0]->p_filesz;
       memcpy(entrance ,zero , phdr[0]->p_memsz - phdr[0]->p_filesz);
-      phdraddr += sizeof(Elf_Phdr);
-      assert(0);
+      
     }
+    phdraddr += sizeof(Elf_Phdr);
     
   }
   return 0;
