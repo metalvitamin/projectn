dcffe6c9ea4113a6863c6aa26b1d0e8416028df2
tracer-ics2020 <tracer@njuics.org>
2020-12-28 15:41:14 +0800

    >  run 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: c1a0515ffd294d459f6c7758f260df8f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  15:41:14 up 1 day, 16:55,  1 user,  load average: 0.51, 0.61, 0.60

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -20,11 +20,11 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   printf("vaddr = 0x%x\n", ehdr.e_entry);
   // printf("phdraddr = 0x%x\n",phdraddr);
   int count = ehdr.e_phnum;
-  Elf32_Phdr phdr = {0};
+  Elf_Phdr phdr = {0};
   for(int i = 0; i < count; i ++){
     putch('\n');putch('\n');putch('\n');
     printf("phdr access = 0x%x\n", phdraddr);
-    ramdisk_read(&phdr, phdraddr, sizeof(Elf32_Phdr));
+    ramdisk_read(&phdr, phdraddr, sizeof(Elf_Phdr));
     printf("offset = 0x%x\n", phdr.p_offset);
     printf("type = %d, vaddr = 0x%x, filesize = 0x%x\n", phdr.p_type, phdr.p_vaddr, phdr.p_filesz);
     if(phdr.p_type == PT_LOAD){
