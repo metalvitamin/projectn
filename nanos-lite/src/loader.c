2eb5b4a88bc11f4720a9cc1007d0383993d4d31f
tracer-ics2020 <tracer@njuics.org>
2020-12-28 15:36:07 +0800

    >  run 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: c1a0515ffd294d459f6c7758f260df8f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  15:36:07 up 1 day, 16:50,  1 user,  load average: 1.64, 0.81, 0.65

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -29,7 +29,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     if(phdr.p_type == PT_LOAD){
       uint8_t buf[phdr.p_filesz];
       ramdisk_read(buf, phdr.p_offset, phdr.p_filesz);
-      //printf("vaddr = %x\n", phdr->p_vaddr);
+      printf("vaddr = %x\n", phdr.p_vaddr);
       memcpy((void *)phdr.p_vaddr, buf, phdr.p_filesz);
       memset((void *)(phdr.p_vaddr + phdr.p_filesz) ,0 , phdr.p_memsz - phdr.p_filesz);
       
