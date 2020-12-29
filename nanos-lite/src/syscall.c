657753bf581cce296a5317ed6bf0b574a945e8b6
tracer-ics2020 <tracer@njuics.org>
2020-12-29 20:59:11 +0800

    >  run 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 474bd846179d46eb9b20d7df71725e54   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  20:59:11 up  1:19,  1 user,  load average: 0.51, 0.57, 0.44

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,7 +5,7 @@ void do_syscall(Context *c) {
   a[0] = c->GPR1;
 
   switch (a[0]) {
-    case 0: halt(a[0]);break;
+    case 0: halt(c->GPR2);break;
     case 1: yield();break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
