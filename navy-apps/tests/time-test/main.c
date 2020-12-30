fa3814376c20f8d3e0d05c92a32d6ff3f0097326
tracer-ics2020 <tracer@njuics.org>
2020-12-30 23:52:59 +0800

    >  run 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: fd3914f7f5eb4f7f863d5d972ae9b253   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  23:52:59 up  7:50,  1 user,  load average: 0.65, 0.58, 0.53

--- navy-apps/tests/time-test/main.c
+++ navy-apps/tests/time-test/main.c
@@ -3,15 +3,19 @@
 #include <sys/time.h>
 
 int main(){
+    struct timeval seconds = {.tv_sec = 0, .tv_usec = 0};
+    int i = 1;
     while (1)
     {
-        struct timeval seconds = {.tv_sec = 0, .tv_usec = 0};
-        gettimeofday(&seconds, NULL);
-        int i = 1;
-        if(seconds.tv_sec > i){
-            printf("hello world at %d seconds!\n",i);
-            i++;
+        while (seconds.tv_sec < i)
+        {
+            gettimeofday(&seconds, NULL);
         }
+        printf("hello world at %d seconds\n", i);
+        i ++;
+        
+        
+        
     }
     return 0;
 }
