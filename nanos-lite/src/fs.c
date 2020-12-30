c7b474b8a1247fc08f375f2f7c216fbfb6c0dd97
tracer-ics2020 <tracer@njuics.org>
2020-12-30 22:21:42 +0800

    >  run 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: fd3914f7f5eb4f7f863d5d972ae9b253   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  22:21:42 up  6:19,  1 user,  load average: 0.43, 0.31, 0.25

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -52,7 +52,7 @@ size_t fs_read(int fd, void *buf, size_t len){
   if(fd < sizeof(file_table)/sizeof(Finfo)){
     size_t maxlen = file_table[fd].size;
     size_t offset = file_table[fd].disk_offset + Foffset[fd];
-    printf("offset = %d, len = %d, size = %d\n",Foffset[fd], len,maxlen);
+    // printf("offset = %d, len = %d, size = %d\n",Foffset[fd], len,maxlen);
     if(Foffset[fd] + len > maxlen){
       len = maxlen - Foffset[fd];
       Log("cause the filesize is fixed, you can just read %d bytes", len);
@@ -80,7 +80,7 @@ size_t fs_write(int fd, const void *buf, size_t len){
   if(fd < sizeof(file_table)/sizeof(Finfo)){
     size_t maxlen = file_table[fd].size;
     size_t offset = file_table[fd].disk_offset + Foffset[fd];
-    printf("offset = %d, len = %d, size = %d\n",Foffset[fd], len,maxlen);
+    // printf("offset = %d, len = %d, size = %d\n",Foffset[fd], len,maxlen);
     if(Foffset[fd] + len > maxlen){
       len = maxlen - Foffset[fd];
       Log("cause the filesize is fixed, you can just read %d bytes", len);
