3780f54da9c7a2b1c87b7800a1df64fbb54fc59b
tracer-ics2020 <tracer@njuics.org>
2021-01-01 11:38:17 +0800

    >  run 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 462f8e5dbaff4b98a1ac9bc44b438a4f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  11:38:17 up 7 min,  1 user,  load average: 1.14, 0.88, 0.44

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -17,7 +17,7 @@ void do_syscall(Context *c) {
     case SYS_exit: halt(a[0]);break;        //???
     case SYS_yield: yield();break;    
     case SYS_open: c->GPR1 = fs_open((char *)a[1],a[2],a[3]); break;     
-    case SYS_read: c->GPR1 = fs_read(a[1],(void *)a[2],a[3]);printf("ret = %d",c->GPR1) ;break;     
+    case SYS_read: c->GPR1 = fs_read(a[1],(void *)a[2],a[3]);break;     
     case SYS_write: c->GPR1 = fs_write(a[1],(void *)a[2],a[3]); break;     
     case SYS_close: c->GPR1 = fs_close(a[1]); break;              
     case SYS_lseek: c->GPR1 = fs_lseek(a[1],a[2],a[3]);break;     
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -16,8 +16,10 @@ uint32_t NDL_GetTicks() {
 }
 
 int NDL_PollEvent(char *buf, int len) {
-  printf("NDL\n");
-  if(read(3 , buf, len) > 0) return 1;
+  // printf("NDL\n");
+  int ret = read(3 , buf, len);
+  printf("ret = %d\n",ret);
+  if(ret != 0 )return 1;
   else return 0;
 }
 
