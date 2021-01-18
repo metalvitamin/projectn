afbededd6338d3eb5adb08cecb76979a3f69b046
tracer-ics2020 <tracer@njuics.org>
2021-01-18 20:29:33 +0800

    >  run 191300032    Static hostname: leigon-ene          Icon name: computer-laptop            Chassis: laptop         Machine ID: f0c3ec0f21304d818433ace7564c993c            Boot ID: 026c94a274124ca9a56e364ab69e745a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-58-generic       Architecture: x86-64  20:29:34 up  4:09,  1 user,  load average: 0.82, 0.86, 0.79

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -47,8 +47,8 @@ void NDL_OpenCanvas(int *w, int *h) {
     // char buf[30];
     // read(open("/proc/dispinfo", 0), buf, 23);
     // sscanf(buf,"%d%d",w,h);
-    screen_w = 400;
-    screen_h = 300;
+    screen_w = (screen_w + *w) / 2;
+    screen_h = (screen_h + *h) / 2;
     printf("width = %d, height = %d",screen_w,screen_h);
   }
   
@@ -75,6 +75,9 @@ int NDL_Init(uint32_t flags) {
   if (getenv("NWM_APP")) {
     evtdev = 3;
   }
+  char buf[30];
+  read(open("/proc/dispinfo", 0), buf, 23);
+  sscanf(buf,"%d%d",&screen_w,&screen_h);
   return 0;
 }
 
